1ab7c463b224d6816020ca77c871aa2c591eed76
Kartik Saini <kartik100saini@gmail.com>
2021-12-21 15:25:24 +0530

    router setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import User from "./components/User";
+import Repos from "./components/Repos";
+import Collaborators from "./components/Collaborators";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <div>Navbar</div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="user" element={<User />} />
+        <Route path="repos" element={<Repos />} />
+        <Route path="collaborators" element={<Collaborators />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
